0e834184fa21606ca710f5a29ddbefc67f12826e
mrMiror <www.reza28902@gmail.com>
2026-05-08 22:56:39 +0330

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="tamplate backend")
+
+@app.get("/")
+async def mainRoute():
+    return {"message":"from mainRoute : Hello frontend and browser ! ! ! "}
