6da6565d3eea6f1a96d1ba04890301200f2687fe
mrMiror <www.reza28902@gmail.com>
2026-06-02 13:22:31 +0330

    chore: migrate dependency management to uv

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from controllers.admin_route import router
+from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI(docs_url=None, redoc_url=None)
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -11,22 +12,11 @@ app.include_router(router=router)
 
 
 @app.get("/scalar", include_in_schema=False)
-async def scalar_html():
-    html_content = """
+def get_scalar_docs():
 
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>Scalar Offline API Reference</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </head>
-    <body>
-    
-    <script id="api-reference" data-url="/openapi.json"></script>
-    
-    <script src="/static/standalone.js"></script>
-    </body>
-    </html>
-    """
-    return HTMLResponse(html_content)
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Scalar API ",
+    )
+
+
